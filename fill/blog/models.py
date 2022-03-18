d6bc8fb0cc738078b0dbc64f8e5585198e81d0c3
leo6556 <91451688+leo6556@users.noreply.github.com>
2022-03-18 14:02:07 +0300

    sn
    cdn

---
+++ fill/blog/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateField(default=timezone.now())
+    published = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.published = timezone.now()
+        self.save()
